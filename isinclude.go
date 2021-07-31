f2a594acf730cffea261ff488221606bbf2f7226
pokrov <al.n.pokrovsky@gmail.com>
2021-08-01 01:14:15 +0300

    task ready

---
+++ isinclude.go
@@ -0,0 +1,83 @@
+/*
+ * Дан упорядоченный массив чисел размером N
+ * Нужно реализовать алгоритм поиска вхождения упорядоченного
+ * подмассива размера M, где M << N
+ */
+package isinclude
+
+// IsInclude проверяет, что array включает subarray
+func IsInclude(array []int, subarray []int) bool {
+	if len(subarray) == 0 {
+		return true
+	}
+
+	firstPossible := findPosBin(array, subarray[0])
+	if firstPossible == -1 {
+		return false
+	}
+
+	cf1 := countDuplicatesOfFirst(array[firstPossible:])
+	cf2 := countDuplicatesOfFirst(subarray)
+	if cf1 < cf2 {
+		return false
+	} else {
+		firstPossible += (cf1 - cf2)
+	}
+
+	return hasEqual(array[firstPossible:], subarray)
+}
+
+// findPosBin выполняет бинарный поиск в отсортированном массиве
+// если элемент не найден возвращает -1, иначе позицию первого вхождения
+func findPosBin(array []int, need int) int {
+	lowPos := 0               // первый индекс
+	highPos := len(array) - 1 // последний индекс
+
+	if (array[lowPos] > need) || (array[highPos] < need) {
+		return -1 // нужное значение не в диапазоне данных
+	}
+
+	for lowPos < highPos {
+		mid := (lowPos + highPos) / 2
+
+		if array[mid] < need {
+			lowPos = mid + 1
+			continue
+		}
+		if array[mid] >= need {
+			highPos = mid
+			continue
+		}
+	}
+
+	if array[lowPos] == need {
+		return lowPos
+	} else {
+		return -1
+	}
+}
+
+// countDuplicatesOfFirst подсчитывает количество повторений
+// первого элемента в упорядоченном массиве
+func countDuplicatesOfFirst(array []int) int {
+	count := 1
+	for array[0] == array[count] {
+		count++
+	}
+	return count
+}
+
+// hasEqual проверяет на равенство subarray и начало array
+// (array может быть больше subarray в отличие от isEqual)
+func hasEqual(array []int, subarray []int) bool {
+	if len(array) < len(subarray) {
+		return false
+	}
+
+	for i, v := range subarray {
+		if v != array[i] {
+			return false
+		}
+	}
+	return true
+}
---
+++ isinclude_test.go
@@ -0,0 +1,29 @@
+package isinclude_test
+
+import (
+	"testing"
+
+	"github.com/alnpokrovsky/isinclude"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsIncludeBasic(t *testing.T) {
+	assert.Equal(t, true, isinclude.IsInclude([]int{1, 2, 3, 5, 7, 9, 11}, []int{}))
+	assert.Equal(t, true, isinclude.IsInclude([]int{1, 2, 3, 5, 7, 9, 11}, []int{3, 5, 7}))
+	assert.Equal(t, false, isinclude.IsInclude([]int{1, 2, 3, 5, 7, 9, 11}, []int{4, 5, 7}))
+}
+
+func TestIsIncludeDuplicates(t *testing.T) {
+	assert.Equal(t, true, isinclude.IsInclude([]int{1, 2, 3, 3, 5, 7, 9, 11}, []int{3, 3, 5, 7}))
+	assert.Equal(t, true, isinclude.IsInclude([]int{1, 2, 3, 3, 3, 5, 7, 9, 11}, []int{3, 3, 5, 7}))
+	assert.Equal(t, true, isinclude.IsInclude([]int{1, 2, 3, 3, 3, 3, 3, 5, 7, 9, 11}, []int{3, 3, 5, 7}))
+	assert.Equal(t, true, isinclude.IsInclude([]int{1, 2, 3, 3, 3, 5, 7, 9, 11}, []int{3, 5, 7}))
+	assert.Equal(t, false, isinclude.IsInclude([]int{1, 2, 3, 3, 3, 5, 5, 7, 9, 11}, []int{3, 5, 7}))
+	assert.Equal(t, false, isinclude.IsInclude([]int{1, 2, 3, 3, 3, 5, 5, 5, 5, 5, 5, 7, 7, 8, 9, 11}, []int{3, 5, 7}))
+}
+
+func TestIsIncludeSpaces(t *testing.T) {
+	assert.Equal(t, false, isinclude.IsInclude([]int{1, 2, 3, 5, 7, 9, 11}, []int{3, 5, 9}))
+	assert.Equal(t, false, isinclude.IsInclude([]int{1, 2, 3, 5, 5, 5, 7, 9, 11}, []int{5, 5, 9}))
+	assert.Equal(t, false, isinclude.IsInclude([]int{1, 2, 3, 5, 7, 9, 11}, []int{3, 11}))
+}
